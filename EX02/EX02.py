c32614e90c649b71a25e260c937e49944a826c23
Kristin <krvare@taltech.ee>
2025-02-07 17:19:21 +0200

    uuwu

---
+++ EX02/EX02.py
@@ -0,0 +1,72 @@
+"""EX02: State Machines."""
+from turtlebot import Robot as robot1
+
+class Robot:
+    """Turtlebot robot."""
+
+    def __init__(self, robot: robot1) -> None:
+        """Class initializer.
+
+        Args:
+            robot (object): An instance of a Turtlebot-like robot interface.
+        """
+        self.robot = robot
+
+    def get_state(self) -> str:
+        """Extract the current state of the robot based on Lidar sensor readings.
+
+        Logic:
+            - This function uses the Lidar range readings to determine the current state
+            of the robot in terms of distance to obstacle directly in front of it.
+            - The state is assigned based on the following conditions:
+                - Very Far: If the range is greater than or equal to 1.5 meters.
+                - Far: If the range is between (inclusive) 1.0 and 1.5 meters.
+                - Near: If the range is between (inclusive) 0.7 and 1.0 meters.
+                - Close: If the range is less than 0.7 meters.
+            - If no new measurement is available from the Lidar sensor, the function
+            will retain the previous state.
+
+        Example:
+            If the Lidar front facing reading returns a distance of 0.65 meters,
+            the robot's state will be "close". If the distance is 1.2 meters, the state
+            will be "far".
+
+        Note:
+            You will have to determine and extract the Lidars front facing direction
+            measurement from the Lidar sensor
+            readings.
+
+        Returns:
+            str: The current state of the robot based on its distance from an obstacle:
+                 ("very far", "far", "near", or "close").
+        """
+
+    def sense(self) -> None:
+        """Gather sensor data.
+
+        Use the robot's sensors to collect data about its environment.
+        This method updates internal state variables based on sensor readings.
+        """
+
+    def plan(self) -> None:
+        """Plan the robot's actions.
+
+        Process the data collected during sensing and decide the next course
+        of action for the robot.
+        """
+
+    def act(self) -> None:
+        """Execute planned actions.
+
+        Perform the actions decided in the planning step, such as moving or
+        interacting with the environment.
+        """
+
+    def spin(self) -> None:
+        """Spin the robot.
+
+        This is the main loop where the robot performs its sense-plan-act cycle.
+        """
+        self.sense()
+        self.plan()
+        self.act()
