36abf7a9895e24b8e23395b4792c196485437dd7
Kristin <krvare@taltech.ee>
2025-02-10 12:33:38 +0200

    def

--- S1/robot.py
+++ S1/robot.py
@@ -1,5 +1,6 @@
 """S1."""
 
+
 class Robot:
     """Turtlebot robot."""
 
@@ -10,6 +11,9 @@ class Robot:
             robot (object): An instance of a Turtlebot-like robot interface.
         """
         self.robot = robot
+        self.lidar_data = None
+        self.cylinder_angle = None
+        self.front_distance = None
 
     def sense(self) -> None:
         """Gather sensor data.
@@ -25,13 +29,15 @@ class Robot:
         Process the data collected during sensing and decide the next course
         of action for the robot.
         """
-        min_distance = min(self.lidar_data)
-        min_index = self.lidar_data.index(min_distance)
+        if self.lidar_data is not None:
+            min_distance = min(self.lidar_data)
+            min_index = self.lidar_data.index(min_distance)
 
-        angle_per_step = 360 / len(self.lidar_data)
-        cylinder_angle = (min_index * angle_per_step) - 90
+            angle_per_step = 360 / len(self.lidar_data)
+            self.cylinder_angle = (min_index * angle_per_step) - 90
 
-        return min_distance, cylinder_angle
+            index = len(self.lidar_data) // 4
+            self.front_distance = self.lidar_data[-index]
 
     def act(self) -> None:
         """Execute planned actions.
@@ -39,20 +45,18 @@ class Robot:
         Perform the actions decided in the planning step, such as moving or
         interacting with the environment.
         """
-        distance, angle = self.sense()
-
-        if distance < 0.3:  # Stop if very close
+        if self.front_distance < 0.3:
             self.robot.set_left_motor_velocity(0)
             self.robot.set_right_motor_velocity(0)
             print("Reached the cylinder!")
 
-        if -10 <= angle <= 10:  # Move straight if directly ahead
+        if -10 <= self.cylinder_angle <= 10:
             self.robot.set_left_motor_velocity(2.0)
             self.robot.set_right_motor_velocity(2.0)
-        elif angle > 10:  # Turn right if cylinder is on the right
+        elif self.cylinder_angle > 10:
             self.robot.set_left_motor_velocity(1.5)
             self.robot.set_right_motor_velocity(2.5)
-        else:  # Turn left if cylinder is on the left
+        else:
             self.robot.set_left_motor_velocity(2.5)
             self.robot.set_right_motor_velocity(1.5)
 
@@ -61,6 +65,7 @@ class Robot:
 
         This is the main loop where the robot performs its sense-plan-act cycle.
         """
+        print("lala")
         self.sense()
-        self.plan()
-        self.act()
+        #self.plan()
+        #self.act()
