5255a2d1ed83cf5ac70bfce19336c9529e4721eb
Kristin <krvare@taltech.ee>
2025-04-07 11:45:15 +0300

    katsetus

--- C2/robot.py
+++ C2/robot.py
@@ -213,7 +213,7 @@ class Robot:
         self.right_velocity = 2.0
         print("SEARCHING:", self.color_order[self.current_color_index])
         if self.color_object_angles:
-            if -0.2 < self.color_object_angles[0] < 0.2:
+            if -0.15 < self.color_object_angles[0] < 0.15:
                 self.left_velocity = 0.0
                 self.right_velocity = 0.0
                 self.state = "approaching"
