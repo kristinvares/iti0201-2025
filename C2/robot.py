bdf738766579613598e6947bacac5f6a34cdedba
Kristin <krvare@taltech.ee>
2025-04-11 10:52:53 +0300

    katsetus

--- C2/robot.py
+++ C2/robot.py
@@ -240,8 +240,8 @@ class Robot:
             self.search_timer = 0
 
     def _handle_approaching(self):
-        self.left_velocity = 3.5
-        self.right_velocity = 3.5
+        self.left_velocity = 2.5
+        self.right_velocity = 2.5
 
         if not self.range_list:
             return
