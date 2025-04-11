4b44db672260a02762e8273390954d2d3e4787bb
Kristin <krvare@taltech.ee>
2025-04-11 14:42:28 +0300

    katsetus

--- C2/robot.py
+++ C2/robot.py
@@ -5,6 +5,7 @@ import numpy as np
 import time
 
 from numpy.ma.core import angle
+from numpy.random.c_distributions import random_standard_exponential_inv_fill
 
 
 class Robot:
@@ -117,13 +118,11 @@ class Robot:
     def _handle_search(self):
         if self.scan_start_angle is None:
             self.scan_start_angle = math.degrees(self.orientation) % 360
-            print(self.scan_start_angle)
             print("Started 360° scan")
 
         self.left_velocity = -1.5
         self.right_velocity = 1.5
         current_angle = math.degrees(self.orientation) % 360
-        print(current_angle)
         if self.color_object_angles:
             if -0.1 < self.color_object_angles[0] < 0.1:
                 front_distance = self._get_front_distance()
@@ -133,7 +132,6 @@ class Robot:
                     print(f"New best target at angle {current_angle:.2f}°, distance {front_distance:.2f}m")
 
         angle_diff = (current_angle - self.scan_start_angle + 360) % 360
-        print(angle_diff)
         if angle_diff > 350 and self.best_target_angle is not None:
             print(f"Scan complete. Best target: {self.best_target_angle:.2f}° at {self.best_target_distance:.2f}m")
             self.state = "approaching"
@@ -163,7 +161,22 @@ class Robot:
 
     def _handle_adjusting(self):
         if self.color_object_angles:
+            for angle in self.color_object_angles:
+                print("angle", angle)
+                ratio = math.pi * 2 / 640
+                print("ratio", ratio)
+                mode = abs(angle) // ratio
+                print("mode", mode)
+                if angle > 0.0:
+                    dist = min(self.lidar[480+mode-2:480+mode+2])
+                    print(dist)
+                else:
+                    print("else")
+                    dist = min(self.lidar[480 - mode - 2:480 - mode + 2])
+                    print(dist)
             cam_angle = self.color_object_angles[0]
+
+
             print(f"Adjusting with camera. Δ{cam_angle:.4f} rad")
 
             # Kitsam piir - täpsem keskele asetamine
